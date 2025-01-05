8cb47f2d380bc1e6595c4a7f9bdf70b27243ab90
isaquliyev <127255683+isaquliyev@users.noreply.github.com>
2025-01-06 01:46:26 +0400

    2381
    I used difference array and cumulative sum for decrease number of iterations. It works :)

--- 2381.py
+++ 2381.py
@@ -1,17 +1,18 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
-        prefix = len(s) * [0];
-        modified_list = []
+        n = len(s)
+        prefix = [0] * (n + 1)
 
-        for shift in shifts:
-            for i in range(shift[0], shift[1] + 1):
-                if shift[2]:
-                    prefix[i] += 1
-                else:
-                    prefix[i] -= 1
+        for start, end, direction in shifts:
+            prefix[start] += 1 if direction == 1 else -1
+            prefix[end + 1] -= 1 if direction == 1 else -1
             
-        for i in range(len(s)):
-            new_char = chr((ord(s[i]) - 97 + prefix[i]) % 26 + 97)
-            modified_list.append(new_char)
-    
-        return ''.join(modified_list)
+        for i in range(1, n):
+            prefix[i] += prefix[i - 1]
+
+        result = []
+        for i in range(n):
+            new_char = chr((ord(s[i]) - ord('a') + prefix[i]) % 26 + ord('a'))
+            result.append(new_char)
+        
+        return ''.join(result)
